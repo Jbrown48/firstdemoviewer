8128438e1be5645cefaa5701105a02df7ccdc681
Jbrown48 <jarrettjbrown@gmail.com>
2023-11-16 12:52:54 -0600

    Update data.js change order of images

--- app-files/data.js
+++ app-files/data.js
@@ -1,133 +1,156 @@
 var APP_DATA = {
-  "scenes": [
+  scenes: [
     {
-      "id": "0-53329058746_8bd09be3f4_b",
-      "name": "53329058746_8bd09be3f4_b",
-      "levels": [
+      id: "0-53024828396_52481b910b_o",
+      name: "53024828396_52481b910b_o",
+      levels: [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
         },
         {
-          "tileSize": 512,
-          "size": 512
-        }
+          tileSize: 512,
+          size: 512,
+        },
+        {
+          tileSize: 512,
+          size: 1024,
+        },
+        {
+          tileSize: 512,
+          size: 2048,
+        },
+        {
+          tileSize: 512,
+          size: 4096,
+        },
       ],
-      "faceSize": 256,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+      faceSize: 3067.5,
+      initialViewParameters: {
+        yaw: 1.0157598949237379,
+        pitch: 0.2512347804165991,
+        fov: 1.5707963267948966,
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      linkHotspots: [
+        {
+          yaw: -2.052077945288527,
+          pitch: 0.046682066415066004,
+          rotation: 5.497787143782138,
+          target: "1-53329067811_a1ed996b64_o",
+        },
+      ],
+      infoHotspots: [
+        {
+          yaw: 3.014189155196542,
+          pitch: 0.43543489549384695,
+          title: "Old Piano",
+          text: "It is missing all the black keys.",
+        },
+        {
+          yaw: -1.3733335719725854,
+          pitch: 0.16250618860627952,
+          title: "TV&nbsp;",
+          text: "Is all black.&nbsp;",
+        },
+        {
+          yaw: 1.3169832744957226,
+          pitch: 0.11522623730775905,
+          title: "Man",
+          text: "Could be Hitler????",
+        },
+      ],
     },
     {
-      "id": "1-53329067811_a1ed996b64_o",
-      "name": "53329067811_a1ed996b64_o",
-      "levels": [
+      id: "1-53329067811_a1ed996b64_o",
+      name: "53329067811_a1ed996b64_o",
+      levels: [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
         },
         {
-          "tileSize": 512,
-          "size": 512
+          tileSize: 512,
+          size: 512,
         },
         {
-          "tileSize": 512,
-          "size": 1024
+          tileSize: 512,
+          size: 1024,
         },
         {
-          "tileSize": 512,
-          "size": 2048
-        }
+          tileSize: 512,
+          size: 2048,
+        },
       ],
-      "faceSize": 1680,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+      faceSize: 1680,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
       },
-      "linkHotspots": [
+      linkHotspots: [
         {
-          "yaw": -0.3539323111990029,
-          "pitch": 0.6584419322398798,
-          "rotation": 0.7853981633974483,
-          "target": "2-53024828396_52481b910b_o"
-        }
+          yaw: -0.3539323111990029,
+          pitch: 0.6584419322398798,
+          rotation: 0.7853981633974483,
+          target: "0-53024828396_52481b910b_o",
+        },
       ],
-      "infoHotspots": []
+      infoHotspots: [],
     },
     {
-      "id": "2-53024828396_52481b910b_o",
-      "name": "53024828396_52481b910b_o",
-      "levels": [
+      id: "2-53329058746_8bd09be3f4_b",
+      name: "53329058746_8bd09be3f4_b",
+      levels: [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
         },
         {
-          "tileSize": 512,
-          "size": 512
+          tileSize: 512,
+          size: 512,
         },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
       ],
-      "faceSize": 3067.5,
-      "initialViewParameters": {
-        "yaw": 1.0157598949237379,
-        "pitch": 0.2512347804165991,
-        "fov": 1.5707963267948966
+      faceSize: 256,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
       },
-      "linkHotspots": [
+      linkHotspots: [],
+      infoHotspots: [],
+    },
+    {
+      id: "3-00000-3607855071_o",
+      name: "00000-3607855071_o",
+      levels: [
         {
-          "yaw": -2.052077945288527,
-          "pitch": 0.046682066415066004,
-          "rotation": 0,
-          "target": "1-53329067811_a1ed996b64_o"
-        }
+          tileSize: 256,
+          size: 256,
+          fallbackOnly: true,
+        },
+        {
+          tileSize: 512,
+          size: 512,
+        },
       ],
-      "infoHotspots": [
-        {
-          "yaw": 3.014189155196542,
-          "pitch": 0.43543489549384695,
-          "title": "Old Piano",
-          "text": "It is missing all the black keys."
-        },
-        {
-          "yaw": -1.3733335719725854,
-          "pitch": 0.16250618860627952,
-          "title": "TV&nbsp;",
-          "text": "Is all black.&nbsp;"
-        },
-        {
-          "yaw": 1.3169832744957226,
-          "pitch": 0.11522623730775905,
-          "title": "Man",
-          "text": "Could be Hitler????"
-        }
-      ]
-    }
+      faceSize: 256,
+      initialViewParameters: {
+        pitch: 0,
+        yaw: 0,
+        fov: 1.5707963267948966,
+      },
+      linkHotspots: [],
+      infoHotspots: [],
+    },
   ],
-  "name": "Project Title",
-  "settings": {
-    "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
-  }
+  name: "Project Title",
+  settings: {
+    mouseViewMode: "drag",
+    autorotateEnabled: true,
+    fullscreenButton: false,
+    viewControlButtons: false,
+  },
 };
